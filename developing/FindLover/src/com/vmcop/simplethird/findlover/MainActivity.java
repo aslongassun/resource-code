37ac4e7625d7c6de5a9f74fd743f2e9973753819
aslongassun <huaquocvinh@gmail.com>
2015-06-26 17:25:08 +0700

    content comment

--- developing/FindLover/src/com/vmcop/simplethird/findlover/MainActivity.java
+++ developing/FindLover/src/com/vmcop/simplethird/findlover/MainActivity.java
@@ -1,5 +1,7 @@
 package com.vmcop.simplethird.findlover;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
@@ -11,6 +13,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -28,8 +31,13 @@ import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.squareup.picasso.Picasso;
 import com.vmcop.simplethird.findlover.constant.ConstantValue;
+import com.vmcop.simplethird.findlover.profileinfoendpoint.Profileinfoendpoint;
+import com.vmcop.simplethird.findlover.profileinfoendpoint.model.CollectionResponseProfileInfo;
+import com.vmcop.simplethird.findlover.profileinfoendpoint.model.ProfileInfo;
 
 
 public class MainActivity extends Activity {
@@ -41,7 +49,11 @@ public class MainActivity extends Activity {
     List<String> permissionNeeds= Arrays.asList("user_birthday", "user_friends", "public_profile");
 
     JSONObject jsonReturn;
-
+    
+	ProfileInfo profileInfo;
+	
+	List<ProfileInfo> listProfileInfo;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,6 +97,8 @@ public class MainActivity extends Activity {
 
     private void initData(){
         jsonReturn = new JSONObject();
+        profileInfo = new ProfileInfo();
+        listProfileInfo = new ArrayList<ProfileInfo>();
     }
     //===========2015/06/04 VMCop=========//
 
@@ -110,9 +124,9 @@ public class MainActivity extends Activity {
         Profile currProfile = Profile.getCurrentProfile();
         Log.d(TAG, "ProfileName" + currProfile.getName());
         // Set Image vao ImageView ImageView yourImageView = (ImageView)findViewById(R.id.You);
-        //setImageProfileForImageView(currProfile.getProfilePictureUri(500, 500).toString(),(ImageView)findViewById(R.id.You));
+        setImageProfileForImageView(currProfile.getProfilePictureUri(500, 500).toString(),(ImageView)findViewById(R.id.You));
         Log.d(TAG, "ProfileUrl" + currProfile.getProfilePictureUri(500, 500).toString());
-        // requestGraphData(loginResult);
+        requestGraphData(loginResult);
 
 
 
@@ -143,8 +157,17 @@ public class MainActivity extends Activity {
                         editor.putString(ConstantValue.MY_SEX,object.optString(ConstantValue.MY_SEX));
                         editor.putString(ConstantValue.MY_BIRTHDAY,object.optString(ConstantValue.MY_BIRTHDAY));
                         editor.putInt(ConstantValue.MY_AGE, calculateAgeFromBirthDay(object.optString(ConstantValue.MY_BIRTHDAY)));
-
+                        // currProfile.getProfilePictureUri(500, 500).toString()
+                        
                         editor.commit();
+                        //------------------------//
+                        profileInfo.setProfileId(object.optString(ConstantValue.MY_PROFILE_ID));
+                        profileInfo.setUserName(object.optString(ConstantValue.MY_NAME));
+                        profileInfo.setUserSex(object.optString(ConstantValue.MY_SEX));
+                        profileInfo.setBirthday(object.optString(ConstantValue.MY_BIRTHDAY));
+                        profileInfo.setUserAge(calculateAgeFromBirthDay(object.optString(ConstantValue.MY_BIRTHDAY)));
+                        //------------------------//
+                        new ProfileInfoTask().execute();
                         //------------------------//
                     }
                 }
@@ -170,11 +193,13 @@ public class MainActivity extends Activity {
     private void findLoveAction(){
         Log.d(TAG, "Find Love!");
         // Tim kiem nguoi thich hop
-        SharedPreferences mySharedPreferencesInfo = getSharedPreferences(ConstantValue.PREFS_NAME, 0);
-        Log.d(TAG, "Age getted:" + mySharedPreferencesInfo.getString(ConstantValue.MY_PROFILE_ID, ""));
-        Log.d(TAG, "Age getted:" + mySharedPreferencesInfo.getInt(ConstantValue.MY_AGE, 0));
+        //SharedPreferences mySharedPreferencesInfo = getSharedPreferences(ConstantValue.PREFS_NAME, 0);
+        //Log.d(TAG, "Age getted:" + mySharedPreferencesInfo.getString(ConstantValue.MY_PROFILE_ID, ""));
+        //Log.d(TAG, "Age getted:" + mySharedPreferencesInfo.getInt(ConstantValue.MY_AGE, 0));
         // Thiet lap cac thong tin tren man hinh
-
+        
+        // Thuc hien tiep action sau khi ham ben duoi thanh cong
+        new ListOfProfileInfoAsyncRetriever().execute();
     }
     // Kiem tra xem facebook user nay co trong db chua
     public Boolean checkFBidExist(String fbId){
@@ -187,7 +212,83 @@ public class MainActivity extends Activity {
     private void getUserDataFromFbId(String fbId){
 
     }
+    
+    /**
+     * AsyncTask for calling Mobile Assistant API for checking into a place (e.g., a store)
+    */
+    private class ProfileInfoTask extends AsyncTask<Void, Void, Void> {
 
+      /**
+       * Calls appropriate CloudEndpoint to indicate that user checked into a place.
+       *
+       * @param params the place where the user is checking in.
+       */
+      @Override
+      protected Void doInBackground(Void... params) {
+        
+    	Profileinfoendpoint.Builder builder = new Profileinfoendpoint.Builder(
+            AndroidHttp.newCompatibleTransport(), new JacksonFactory(),
+            null);
+            
+        builder = CloudEndpointUtils.updateBuilder(builder);
+
+        Profileinfoendpoint endpoint = builder.build();
+        
+
+        try {
+          endpoint.insertProfileInfo(profileInfo).execute();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+
+        return null;
+      }
+    }
+    
+    /**
+     * AsyncTask for retrieving the list of places (e.g., stores) and updating the
+     * corresponding results list.
+     */
+    private class ListOfProfileInfoAsyncRetriever extends AsyncTask<Void, Void, CollectionResponseProfileInfo> {
+
+      @Override
+      protected CollectionResponseProfileInfo doInBackground(Void... params) {
+
+
+    	Profileinfoendpoint.Builder endpointBuilder = new Profileinfoendpoint.Builder(
+            AndroidHttp.newCompatibleTransport(), new JacksonFactory(), null);
+       
+        endpointBuilder = CloudEndpointUtils.updateBuilder(endpointBuilder);
+
+
+        CollectionResponseProfileInfo result;
+
+        Profileinfoendpoint endpoint = endpointBuilder.build();
+
+        try {
+          result = endpoint.listProfileInfo().execute();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+          result = null;
+        }
+        return result;
+      }
+
+      @Override
+      protected void onPostExecute(CollectionResponseProfileInfo result) {
+    	  listProfileInfo = result.getItems();
+    	  Log.d(TAG, "onPostExecute_listProfileInfo:" + listProfileInfo.size());
+    	  matchLove();
+      }
+      
+    }
+    
+    private void matchLove(){
+    	
+    }
+    
     //====================================//
 
     @Override
@@ -212,6 +313,5 @@ public class MainActivity extends Activity {
         super.onPause();
         AppEventsLogger.deactivateApp(this);
     }
-
 
 }
---
+++ developing/backup_source/FindLover/gen/com/vmcop/simplethird/findlover/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.vmcop.simplethird.findlover;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
